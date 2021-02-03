6400e75b4a331a870710c764a78ad26beb346cec
David Billups <davidtbillups@gmail.com>
2021-02-03 14:29:32 -0500

    finished NP selector

--- scripts/parks/parkItineraryItem.js
+++ scripts/parks/parkItineraryItem.js
@@ -3,8 +3,16 @@
 const contentTarget = document.querySelector("#park-container")
 
 export const parkHTML = (selectedPark) => {
+    console.log(selectedPark)
+    const imageNum = Math.floor(Math.random()*selectedPark.images.length);
+    console.log(imageNum)
     contentTarget.innerHTML = `
         <p>${selectedPark.fullName}</p>
-        <img src="${selectedPark.images[1].url}" alt="${selectedPark.images[1].altText}">`
+        <div class="overflow-hidden">
+        <div class="image-container w-100 img-fluid hero-img position-relative bottom-50 right-150">
+        <img src="${selectedPark.images[imageNum].url}" alt="${selectedPark.images[imageNum].altText}"></div></div>`
     console.log(contentTarget.innerHTML)
 }
+
+// alt="${selectedPark.images[imageNum].altText}" class="mx-auto"
+/* <div class="image-container" style="background-image: url("${selectedPark.images[imageNum].url}")></div>` */
