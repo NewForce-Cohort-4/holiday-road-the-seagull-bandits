918bdc07a2f16d07c970f72acf01555f35ccbd47
Wes <wrharrison23@gmail.com>
2021-02-03 13:20:23 -0500

    finished rendering eateries list and eatery details

---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,13 @@
+let eateries = []
+
+export const useEateries = () => eateries.slice()
+
+export const getEateries = (id) => {
+  
+    return fetch("http://holidayroad.nss.team/eateries")
+      .then((response) => response.json())
+      .then((parsedEateries) => {
+        eateries = parsedEateries;
+      }); 
+  
+}
