f72d56300a306793132c170f103c2e0a2cfe8d71
Wes <wrharrison23@gmail.com>
2021-02-04 15:58:18 -0500

    weather printing finished

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -3,6 +3,14 @@ import { settings } from "../Settings.js";
 
 let weather = {};
 
+// transforms unix UTC to EST
+const buildDate = (unix) => {
+  let millisecs = unix * 1000;
+  let dateObject = new Date(millisecs)
+  return dateObject.toLocaleString("en-US", {weekday: "long"})
+}
+
+// Fetches weather data
 export const getWeather = (lat, long) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&appid=${settings.weatherKey}&units=imperial`
@@ -17,6 +25,9 @@ export const getWeather = (lat, long) => {
 
 
 const weatherContainer = document.querySelector("#weather-container");
+
+
+// Prints weather to DOM
 export const weatherList = (lat,long) => {;
   getWeather(lat, long).then(()=> {
     let weatherHTMLString = "";
@@ -27,8 +38,78 @@ export const weatherList = (lat,long) => {;
   })
 };
 
+
 const buildWeatherString = (weatherObject) => {
   return `
-  <p>${weatherObject.daily[0].dt.toLocaleString()}</p>
+  
+    <div class="card">
+    <h3 class="card-title weather-day">${buildDate(weatherObject.daily[1].dt)}</h3>
+    <img src="http://openweathermap.org/img/wn/${
+      weatherObject.daily[1].weather[0].icon
+    }@2x.png" class="card-img-top">
+    <p class="card-text"><strong>Forecast: </strong>${
+      weatherObject.daily[1].weather[0].description
+    }</p>
+    <p class="card-text"><strong>High:</strong> ${weatherObject.daily[1].temp.max}&deg;</p>
+    <p class="card-text"><strong>Low:</strong> ${weatherObject.daily[1].temp.min}&deg;</p>
+    </div>
+  
+
+  
+    <div class="card">
+    <h3 class="card-title weather-day">${buildDate(weatherObject.daily[2].dt)}</h3>
+    <img src="http://openweathermap.org/img/wn/${
+      weatherObject.daily[2].weather[0].icon
+    }@2x.png" class="card-img-top">
+    <p class="card-text"><strong>Forecast: </strong>${
+      weatherObject.daily[2].weather[0].description
+    }</p>
+    <p class="card-text"><strong>High:</strong> ${weatherObject.daily[2].temp.max}&deg;</p>
+    <p class="card-text"><strong>Low:</strong> ${weatherObject.daily[2].temp.min}&deg;</p>
+    </div>
+  
+
+  
+    <div class="card">
+    <h3 class="card-title weather-day">${buildDate(weatherObject.daily[3].dt)}</h3>
+    <img src="http://openweathermap.org/img/wn/${
+      weatherObject.daily[3].weather[0].icon
+    }@2x.png" class="card-img-top">
+    <p class="card-text"><strong>Forecast: </strong>${
+      weatherObject.daily[3].weather[0].description
+    }</p>
+    <p class="card-text"><strong>High:</strong> ${weatherObject.daily[3].temp.max}&deg;</p>
+    <p class="card-text"><strong>Low:</strong> ${weatherObject.daily[3].temp.min}&deg;</p>
+    </div>
+  
+
+  
+    <div class="card">
+    <h3 class="card-title weather-day">${buildDate(weatherObject.daily[4].dt)}</h3>
+    <img src="http://openweathermap.org/img/wn/${
+      weatherObject.daily[4].weather[0].icon
+    }@2x.png" class="card-img-top">
+    <p class="card-text"><strong>Forecast: </strong>${
+      weatherObject.daily[4].weather[0].description
+    }</p>
+    <p class="card-text"><strong>High:</strong> ${weatherObject.daily[4].temp.max}&deg;</p>
+    <p class="card-text"><strong>Low:</strong> ${weatherObject.daily[4].temp.min}&deg;</p>
+    </div>
+  
+
+  
+    <div class="card">
+    <h3 class="card-title weather-day">${buildDate(weatherObject.daily[5].dt)}</h3>
+    <img src="http://openweathermap.org/img/wn/${
+      weatherObject.daily[5].weather[0].icon
+    }@2x.png" class="card-img-top">
+    <p class="card-text"><strong>Forecast: </strong>${
+      weatherObject.daily[5].weather[0].description
+    }</p>
+    <p class="card-text"><strong>High:</strong> ${weatherObject.daily[5].temp.max}&deg;</p>
+    <p class="card-text"><strong>Low:</strong> ${weatherObject.daily[5].temp.min}&deg;</p>
+    </div>
+  
   `;
 };
+// "";
