209fe210c3c6d7c1a635dd02601b171b01f92603
jon-xo <jonn@hey.com>
2021-02-05 09:11:42 -0500

    Revert "Added direction provider, started work on navigation function." Changes incorrectly made on wrong branch.
    This reverts commit ec2ae25477d137ca08ddd88a2b50ee426c0a64e2.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-import { parksSelector } from './parks/ParkSelector.js'
-import {getEateries} from "./eateries/EateryProvider.js"
-import {renderEateryDropdown} from "./eateries/EateryList.js"
 import { attractionSelect } from "./attractions/AttractionSelect.js";
 import { findAttraction } from "./attractions/attractionItinerary.js";
-import {  } from "./directions/printDirections.js";
 
 // Make fetch call and build drop down list.
 attractionSelect();
 
 // Make fetch call and loop through entries to print drop-down list selection to the DOM container.
 findAttraction();
+
+import { parksSelector } from './parks/ParkSelector.js'
+import {getEateries} from "./eateries/EateryProvider.js"
+import {renderEateryDropdown} from "./eateries/EateryList.js"
 
 renderEateryDropdown()
 parksSelector()
