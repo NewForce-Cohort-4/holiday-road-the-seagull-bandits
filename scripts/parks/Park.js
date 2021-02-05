8eb3bb35abf4447bbc8ce3b713aa053e090c72a5
David Billups <davidtbillups@gmail.com>
2021-02-05 16:49:36 -0500

    adjusted layout of data columns and added functionality to delete and view buttons

--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -4,19 +4,18 @@ const contentTarget = document.querySelector("#park-container")
 
 // Create and export a function to generate the HTML once the user selects a park
 export const parkHTML = (selectedPark) => {
-
     // Randomly generate an indice to select one of the images provided by the NPS API
     const imageNum = Math.floor(Math.random()*selectedPark.images.length);
     
     // Build the HTML and assign it to the contentTarget
     contentTarget.innerHTML = `
-        <div class="card">
+        <div class="card bg-light text-dark">
             <img src="${selectedPark.images[imageNum].url}" class="card-img-top" alt="${selectedPark.images[imageNum].altText}">
+            <div class="card-header mt-1"><h3>${selectedPark.fullName}</h3></div>
             <div class="card-body">
-                <h5 class="card-title">${selectedPark.fullName}</h5>
                 <p class="card-text">${selectedPark.states}</p>
                 <div id="details-container-parks">
-                    <button class="parks-detail-button" id="parks-detail--${selectedPark.parkCode}">Details..</button>
+                    <button class="parks-detail-button btn btn-primary" id="parks-detail--${selectedPark.parkCode}">Details..</button>
                 </div>
             </div>
         </div>  
@@ -25,6 +24,7 @@ export const parkHTML = (selectedPark) => {
 
 // Create and export a function to generate the HTML for the park's details once the user clicks the "Details" button.
 export const parkDetails = (selectedPark) => {
+    console.log(selectedPark)
     document.querySelector("#details-container-parks").innerHTML = `
     <p style="text-align: left;">${selectedPark.description}</p>
     `
