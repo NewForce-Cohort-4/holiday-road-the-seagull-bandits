02d05207370af1d782cf3616568c9f004391a3cf
David Billups <davidtbillups@gmail.com>
2021-02-05 10:05:33 -0500

    created accordion for saved itineraries aside

--- scripts/itineraries/Itinerary.js
+++ scripts/itineraries/Itinerary.js
@@ -5,45 +5,30 @@
 export const printItinerary = (itinerary) => {
 
     return `
-    <div class="card" style="width: 18rem;">
-    <img src="${itinerary.park.images[0].url}" class="card-img-top" alt="${itinerary.park.images[0].altText}">
-    <div class="card-body">
-      <h5 class="card-title">Trip to ${itinerary.park.name}</h5>
-      <p class="card-text">${itinerary.park.description}</p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">${itinerary.eatery.businessName}</li>
-      <li class="list-group-item">${itinerary.attraction.name}</li>
-    </ul>
-    <div class="card-body">
-        <a href="#" class="btn btn-primary card-link">View Itinerary</a>
-        <a href="#" class="btn btn-primary card-link">Delete Itinerary</a>
-    </div>
-    </div>
-    
-    `
-}
-
-/*
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${itinerary.id}" aria-expanded="true" aria-controls="collapse-${itinerary.id}">
+      Trip to ${itinerary.park.name}
+      </button>
+    </h2>
+    <div id="collapse-${itinerary.id}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion">
+      <div class="accordion-body">
+        <div class="card" style="width: 18rem;">
+          <img src="${itinerary.park.images[0].url}" class="card-img-top" alt="${itinerary.park.images[0].altText}">
+          <div class="card-body">
+            <p class="card-text">${itinerary.park.description}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${itinerary.eatery.businessName}</li>
+            <li class="list-group-item">${itinerary.attraction.name}</li>
+          </ul>
+          <div class="card-body">
+              <a href="#" class="btn btn-primary card-link">View Itinerary</a>
+              <a href="#" class="btn btn-primary card-link">Delete Itinerary</a>
+          </div>
+        </div>  
+      </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </a>
-</div>
-*/
+    `
+}
--- scripts/itineraries/ItineraryList.js
+++ scripts/itineraries/ItineraryList.js
@@ -8,12 +8,11 @@ let contentTarget = document.querySelector("#aside-saved-itineraries")
 export const itineraryList = () => {
     getItineraries().then(() => {
         let itineraries = useItineraries();
-        // contentTarget.innerHTML = '<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner"><div class="carousel-item active">'
+        contentTarget.innerHTML = '<div class="accordion" id="accordion">'
 
         itineraries.forEach(itinerary => {
             contentTarget.innerHTML += printItinerary(itinerary)
         })
-        // contentTarget.innerHTML += '<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>'
+        contentTarget.innerHTML += '</div>'
     })
 }
-
